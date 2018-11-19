085bc8195249708af9d62e6a3e765c0da0cb0854
gitsobek <sobus.piotr@gmail.com>
2018-11-19 14:45:56 +0100

    Add production config

---
+++ config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID: '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
+    googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
+    mongoURI: 'mongodb://sobek:test123@ds159293.mlab.com:59293/postmeme_dev',
+    cookieKey: '123123123'
+}
