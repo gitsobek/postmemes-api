deda7af9c36cd9bbb44899c9d32cc4b1630103d0
gitsobek <sobus.piotr@gmail.com>
2018-11-19 14:37:04 +0100

    Change port, add engines for heroku deployment

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(passport.session())
 require('./routes/auth')(app)
 require('./routes/posts')(app)
 
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
     console.log('Listening on port', PORT)
 })
